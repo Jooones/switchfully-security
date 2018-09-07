5b8be92e9c15c78a6f91bc37b5375aeaba7a48fb
Jooones <jonathanV@bitbucket.org>
2018-09-07 11:09:03 +0200

    configure basic authentication with spring

--- src/main/java/com/cegeka/switchfully/security/CommonConfig.java
+++ src/main/java/com/cegeka/switchfully/security/CommonConfig.java
@@ -1,16 +1,26 @@
 package com.cegeka.switchfully.security;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
 
 import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.ANY;
 import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.NONE;
 
 @Configuration
-public class CommonConfig {
+@EnableWebSecurity
+public class CommonConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private AuthenticationEntryPoint authEntryPoint;
 
     @Bean
     @Primary
@@ -25,5 +35,23 @@ public class CommonConfig {
                         .withCreatorVisibility(NONE)
         );
         return mapper;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests()
+                .anyRequest().authenticated()
+                .and().httpBasic()
+                .authenticationEntryPoint(authEntryPoint);
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("JMILLER").password("$2a$04$BeY/BoZB6ytWyi3Mj0Y4xuevwKvZgv91yVMlTzirZQwkgjlhU75PC").roles("USER");
+    }
+
+    @Bean
+    public BCryptPasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
     }
 }
